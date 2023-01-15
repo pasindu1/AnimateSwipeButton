afebd9cc0a5aed4fba549e33e964729f3a948f12
Pasindu Navod <pasindu@rhino-partners.com>
2023-01-15 13:51:07 +0530

    swipeable animated button

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+import SwipeButton from './src/components/SwipeButton';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <SwipeButton onToggle={() => {}}/>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3f3f4e',
+  },
+});
+
+export default App;
